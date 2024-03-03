15f5297a10cddeb19fd864f8bc1b75d6deac4c64
ANGEL GARCIA GUERRA <angelgargue14@gmail.com>
2024-03-03 12:38:40 +0100

    Primera subida del index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+    <link REL=StyleSheet HREF="style.css" TYPE="text/css" MEDIA=screen>
+    <script src="main.js" type="text/javascript"></script>
+    <title>Lotty Shop</title>
+</head>
+<body>
+	
+</body>
+</html>
