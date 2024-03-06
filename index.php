ff6ae4cba3a54d3439454d8cf8244ccdfe7c0e97
Ángel García Guerra <110335931+Angelgargue14@users.noreply.github.com>
2024-03-06 16:51:28 +0100

    fuentes

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
 <head>
 	<meta charset="UTF-8">
     <link REL=StyleSheet HREF="style.css" TYPE="text/css" MEDIA=screen>
+    <link REL=StyleSheet HREF="fint-icons/remixicon.css" TYPE="text/css" MEDIA=screen>
     <script src="main.js" type="text/javascript"></script>
     <title>Lotty Shop</title>
 </head>
 <body>
 	<header>
-		<p>estamos sin conciliaciones SNCE</p>
+		<button><i class="ri-menu-2-fill"></i></button>
 	</header>
 </body>
 </html>
