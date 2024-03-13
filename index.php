7c7ab408691b1a5a8dbbff42d76d2ddf8f02fadf
Ángel García Guerra <110335931+Angelgargue14@users.noreply.github.com>
2024-03-13 16:14:44 +0100

    login y cesta1

--- index.php
+++ index.php
@@ -13,14 +13,15 @@
         <div class="headers" id="header-top">
         	<img src="img/headers/Logo-tg1.png" name="logo" id="logo">
             
-            <div id="loginYcesta">
-        	<span class="ri-account-circle-line">
-        	<span class="ri-shopping-cart-line">
-        	</div>
+
         </div>
 
         <div class="headers" id="header-bottom">
         	<button id="menu"><span class="ri-menu-line">&nbsp;&nbsp;Navega y se libre</span></button>
+            <div id="loginYcesta">
+            <button><span class="ri-account-circle-line"> Mi Cuenta </span></button>
+            <button><span class="ri-shopping-cart-line"> Mi Cesta </span></button>
+            </div>
         </div>
 
 	</header>
